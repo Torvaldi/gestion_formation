a8b5d16873ea87d909e2cabb60eab46acaf2ef08
Quentin LAURENT <ql2697@gmail.com>
2019-04-27 14:35:22 +0200

    :pushpin: Setted manifest

--- __manifest__.py
+++ __manifest__.py
@@ -1,23 +1,22 @@
 # -*- coding: utf-8 -*-
 {
-    'name': "gestion_formation",
+    'name': "Gestion des formations",
 
     'summary': """
-        Short (1 phrase/line) summary of the module's purpose, used as
-        subtitle on modules listing or apps.openerp.com""",
+        Ce module est destiné pour gérer un centre de formation.""",
 
     'description': """
-        Long description of module's purpose
+        Ce module est destiné pour gérer un centre de formation.
     """,
 
-    'author': "My Company",
+    'author': "Quentin LAURENT",
     'website': "http://www.yourcompany.com",
 
     # Categories can be used to filter modules in modules listing
     # Check https://github.com/odoo/odoo/blob/12.0/odoo/addons/base/data/ir_module_category_data.xml
     # for the full list
-    'category': 'Uncategorized',
-    'version': '0.1',
+    'category': 'formations',
+    'version': '1.0',
 
     # any module necessary for this one to work correctly
     'depends': ['base', 'project'],
